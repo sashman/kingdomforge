9838c893bc856920d9ad9e54a6d24812a54127c1
Sash Man <sashman90.gmail.com>
2011-07-17 21:56:13 +0100

    added basic ajax stuff for map loader, but it doesnae work yet :P

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,17 @@
 class GameController < ApplicationController
-  def index
-  end
+	def index
+	end
 
+	def get_map
+		global_x = #{params['global_x']}
+		global_y = #{params['global_y']}
+		out = ""
+		begin
+			file = File.new("public/data/map/" + global_x + "_" + global_y + ".xml", "rb")
+			out = file.read
+			file.close
+		rescue => e
+			puts "***No map file found***"
+	end
+  
 end
