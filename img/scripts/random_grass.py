8d2be2b6b13c3a053a59e8d5c58531ec74a0f8ca
sashman <sashman90@gmail.com>
2013-04-24 19:19:18 +0100

    added new grass

--- img/scripts/random_grass.py
+++ img/scripts/random_grass.py
@@ -1,13 +1,15 @@
 import PIL, Image, ImageDraw, random
 
 image_file = "grass.png"
+height = 32
+width = 32
 
-newImage = Image.new("RGB", (32,32), (0,136,0))
+newImage = Image.new("RGB", (width,height), (0,136,0))
 draw = ImageDraw.Draw(newImage)
 
-for i in range(random.randint(30,70)):
-	draw.point((random.randint(0,newImage.x),random.randint(0,newImage.y)), 
-		fill = (0,random.randint(140,200),0))
+for i in range(random.randint(100,300)):
+	draw.point((random.randint(0,width),random.randint(0,height)), 
+		fill = (0,random.randint(160,200),0))
 
 
 newImage.save(image_file)
