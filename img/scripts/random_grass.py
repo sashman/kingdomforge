7408d4c880698eaf51d0257d95cc33c5815151ad
sashman <sashman90@gmail.com>
2013-05-15 21:40:26 +0100

    added large grass sprites

--- img/scripts/random_grass.py
+++ img/scripts/random_grass.py
@@ -1,13 +1,21 @@
-import PIL, Image, ImageDraw, random
+import sys, os, re, PIL, Image, ImageDraw, random
 
-image_file = "grass.png"
-height = 64
-width = 64
+if(len(sys.argv) < 2):
+    print "Usage " + str(sys.argv[0]) + " <size>"
+    sys.exit(0)
+
+files = os.listdir(".")
+grass_count = len([f for f in files if(re.match("^grass.*\.png", f))])
+
+
+image_file = "grass"+str(grass_count)+".png"
+height = int(sys.argv[1])
+width = int(sys.argv[1])
 
 newImage = Image.new("RGB", (width,height), (0,136,0))
 draw = ImageDraw.Draw(newImage)
 
-for i in range(random.randint(600,1000)):
+for i in range(random.randint(max(height,width)*10,max(height,width)*15)):
 	draw.point((random.randint(0,width),random.randint(0,height)), 
 		fill = (0,random.randint(160,200),0))
 
