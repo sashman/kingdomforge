69227f1edc75b6314039eb4902096b1dabf4d112
Stuart Robertson <stooie.robertson@gmail.com>
2013-06-11 22:04:14 +0100

    Added two distance algorithms that will generate different wave patterns on the water tiles.

--- img/scripts/generate_water_tile.py
+++ img/scripts/generate_water_tile.py
@@ -13,14 +13,46 @@ Created on May 27, 2013
 from PIL import Image
 import random
 import math
+
+################################################################################
+# Distance functions
+################################################################################
+
+def euclidean_distance(v1, v2):
+    '''
+    Calculates the euclidean distance between two vectors.
+        
+    @param v1: Tuple of x co-ordinate and y co-ordinate of current vector (x,y).
+    @param v1: Tuple of x co-ordinate and y co-ordinate of distant vector (x,y).
+        
+    @return: Euclidean distance between two vectors.
+    '''
+    return math.hypot(v1[0] - v2[0], v1[1] - v2[1])
+
+def manhattan_distance(v1, v2):
+    '''
+    Calculates the manhattan distance between two vectors.
+        
+    @param v1: Tuple of x co-ordinate and y co-ordinate of current vector (x,y).
+    @param v1: Tuple of x co-ordinate and y co-ordinate of distant vector (x,y).
+        
+    @return: Manhattan distance between two vectors.
+    '''
+    return abs((v1[0] - v2[0])) + abs((v1[1] - v2[1]))
+
+################################################################################
+# Tile generation
+################################################################################
  
-def generate_water_tile(width, height, num_cells):
+def generate_water_tile(width, height, num_cells, distance=euclidean_distance):
     '''
     Generates a water tile and saves the output to water_tile.png.
         
     @param width: Image width in pixels.
     @param height: Image height in pixels.
     @param num_cell: Number of cells in the voronoi diagram.
+    @param distance: Distance function to calculate the distance between two 
+    vectors.
     '''
     image = Image.new("RGB", (width, height))
     putpixel = image.putpixel
@@ -40,8 +72,7 @@ def generate_water_tile(width, height, num_cells):
         for x in xrange(imgx):
             # dmin is the Euclidean distance, the length of the vector from the
             # original point to (x, y).
-            # TODO: Add a paramter to pass in another distance function.
-            dmin = math.hypot(imgx-1, imgy-1)
+            dmin = distance((imgx, imgy), (1, 1))
             j = -1
             # For each cell in the voronoi diagram, if dmin is not greater than
             # the distance to the other points then dmin belongs to the point of
@@ -49,7 +80,7 @@ def generate_water_tile(width, height, num_cells):
             for i in xrange(num_cells):
                 point = voronoi_points[i]
                 # Distance to the next point.
-                d = math.hypot(point[0]-x, point[1]-y)
+                d = distance(point, (x, y))
                 if d < dmin:
                     dmin = d
                     j = i
