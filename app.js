ff862752bc7eadb069467375c5ea9587df2cc89c
Angry-Potato <liam.humphreys1989@gmail.com>
2015-04-09 13:41:43 +0100

    added router to handle express requests

--- app.js
+++ app.js
@@ -2,46 +2,41 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var PlayerCollection = require('./modules/player_collection');
+var ExpressRouter = require('./modules/express_router');
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-app.get('/*', function(req, res, next) {
-    var file = req.params[0];
-    console.log('\t :: Express :: file requested : ' + file);
-    res.sendFile(__dirname + '/' + file);
-});
+var expressRouter = new ExpressRouter(app);
 
 
 server.listen(4004);
+
 var playerinfos = new PlayerCollection();
 var sockets = [];
 
 io.on('connection', function(socket) {
-    socket.emit('connected', {
-        id: playerinfos.playerCount(),
-        players: playerinfos.allPlayers()
-    });
+	socket.emit('connected', {
+		id: playerinfos.playerCount(),
+		players: playerinfos.allPlayers()
+	});
 
-    socket.join("players");
-    sockets.push(socket);
+	socket.join("players");
+	sockets.push(socket);
 
-    socket.on('createdplayer', function(data) {
-        playerinfos.addPlayer(data);
-        io.to("players").emit("newplayer", data);
-    });
+	socket.on('createdplayer', function(data) {
+		playerinfos.addPlayer(data);
+		io.to("players").emit("newplayer", data);
+	});
 
-    socket.on('playermoved', function(data) {
-        playerinfos.addPlayer(data);
-        io.to("players").emit("playermoved", data);
-    });
+	socket.on('playermoved', function(data) {
+		playerinfos.addPlayer(data);
+		io.to("players").emit("playermoved", data);
+	});
 
-    socket.on('ping', function(data) {
-        console.log(data);
-        var server_time = (new Date).getTime();
-        socket.emit('pong', {
-            'ping_time': server_time - data,
-            'server_time': server_time
-        });
-    });
+	socket.on('ping', function(data) {
+		console.log(data);
+		var server_time = (new Date).getTime();
+		socket.emit('pong', {
+			'ping_time': server_time - data,
+			'server_time': server_time
+		});
+	});
 });
